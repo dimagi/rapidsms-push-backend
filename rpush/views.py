d4d0ad36fa5229d6e890da0a67aab7653fb3c0a4
Cory Zue <czue@dimagi.com>
2014-02-28 12:51:05 -0600

    make sms processing asynchronous

--- rpush/views.py
+++ rpush/views.py
@@ -1,15 +1,21 @@
-from django.http import HttpResponse
-
 from threadless_router.backends.http.views import BaseHttpBackendView
+from threadless_router.celery.tasks import IncomingTask
 from rpush.forms import PushForm
 from lxml import etree
-from django.http import QueryDict, HttpRequest
+from django.http import QueryDict, HttpRequest, HttpResponse
+
 
 class PushBackendView(BaseHttpBackendView):
     """ Backend view for handling inbound SMSes from Kannel """
 
     http_method_names = ['post']
     form_class = PushForm
+
+    def form_valid(self, form):
+        self.debug('form is valid')
+        IncomingTask.delay(self.backend_name, **form.get_incoming_data())
+        return HttpResponse('OK')
+
 
     def get_form_kwargs(self):
         xml = etree.fromstring(HttpRequest.read(self.request))
